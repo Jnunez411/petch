3e8df38087a658f399f948056a533c69c9cb2afe
Jonathan Nunez <jonathannunez@nevada.unr.edu>
2025-11-12 21:46:24 -0800

    back end and webclient setup

---
+++ petch-api/src/main/java/project/petch/petch_api/PetchApiApplication.java
@@ -0,0 +1,13 @@
+package project.petch.petch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetchApiApplication.class, args);
+	}
+
+}
---
+++ petch-api/src/test/java/project/petch/petch_api/PetchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.petch.petch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

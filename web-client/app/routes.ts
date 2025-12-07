9582964266fc5f0a183df7fb22d626ae9c5502af
Luis Carrillo <luiscarrillo@Mac.lan>
2025-12-06 23:58:35 -0800

    Split profile into adopter/vendor routes and update routing

--- web-client/app/routes.ts
+++ web-client/app/routes.ts
@@ -6,6 +6,8 @@ export default [
   route("signup", "routes/petch+/signup.tsx"),
   route("logout", "routes/petch+/logout.tsx"),
   route("profile", "routes/petch+/profile.tsx"),
+  route("profile/adopter", "routes/petch+/profile.adopter.tsx"),
+  route("profile/vendor", "routes/petch+/profile.vendor.tsx"),
   route("pets", "routes/petch+/pets.tsx"),
   route("pets/create", "routes/petch+/createListing.tsx"),
   route("pets/:id", "routes/petch+/pet.$id.tsx"), // so the $id is how it queries the pet by its id
